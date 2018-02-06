03025e09616840b4adc5dc730df76cc8de13ce64
DercilioFontes <dercilioafontes@gmail.com>
2018-02-06 22:21:48 +0000

    Refactored lookup_people.js to separated functions and checked ESLint issues.

--- lookup_people.js
+++ lookup_people.js
@@ -12,21 +12,33 @@ const client = new pg.Client({
 
 const searchWord = process.argv[2];
 
+// Outputs Seach Results
+function outputResults(rows) {
+
+  console.log(`Found ${rows.length} person(s) by the name '${searchWord}':`);
+
+  rows.forEach( function(row) {
+    const id = row.id;
+    const firstName = row.first_name;
+    const lastName = row.last_name;
+    const birthdate = row.birthdate;
+
+    console.log(`- ${id}: ${firstName} ${lastName}, born '${birthdate.toLocaleDateString()}'`);
+  
+  });
+}
+
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  console.log("Searching ...")
+  console.log("Searching ...");
   client.query(`SELECT * FROM famous_people WHERE last_name LIKE $1::text`, [searchWord], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    const id = result.rows[0].id;
-    const first_name = result.rows[0].first_name;
-    const last_name = result.rows[0].last_name;
-    const birthdate = result.rows[0].birthdate;
 
-    console.log(`- ${id}: ${first_name} ${last_name}, born '${birthdate.toLocaleDateString()}'`);
+    outputResults(result.rows);
     client.end();
   });
 });
